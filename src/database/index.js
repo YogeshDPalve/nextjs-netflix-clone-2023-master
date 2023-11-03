bbd850787ee9f1b3f0de07e5bf50f341f1c04059
Yogesh Palve <100296461+YogeshDPalve@users.noreply.github.com>
2023-11-03 09:05:13 +0530

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectToDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://dnyptushar:dnyptushar@netflixclone.wbko6pl.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://dnyptushar:dnyptushar@netflixclone.wbko6pl.mongodb.net/"
     );
 
     console.log("mongodb is connected");
