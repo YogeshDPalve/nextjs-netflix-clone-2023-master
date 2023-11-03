b325c7be65707f593be44685761184a431a76127
Yogesh <dnyyogsh@gmail.com>
2023-11-03 08:48:24 +0530

    add

---
+++ src/database/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://dnyptushar:dnyptushar@netflixclone.wbko6pl.mongodb.net/?retryWrites=true&w=majority"
+    );
+
+    console.log("mongodb is connected");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default connectToDB;
